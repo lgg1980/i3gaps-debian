1be5b9d7b7f134d7133ec8f51bde572a1c64a6a6
lgg1980 <63470677+lgg1980@users.noreply.github.com>
2022-09-18 00:38:15 +0200

    Create personal.sh

---
+++ personal.sh
@@ -0,0 +1,11 @@
+sudo apt install -y cifs-utils hfsplus hfsutils hfsutils-tcltk gufw gparted bleachbit gdebi dosbox apt-transport-https
+
+sudo apt install -y rar p7zip-rar xarchiver p7zip-full unace zip unzip sharutils uudeview mpack arj cabextract file-roller zstd
+sudo apt install -y xclip libnotify-bin
+
+sudo apt install -y clementine qbittorrent kid3 vlc youtube-dl audacity kdenlive handbrake
+sudo apt install -y obs-studio simplescreenrecorder
+
+sudo apt install -y filezilla keepassxc thunderbird thunderbird-l10n-es-es webext-tbsync telegram-desktop remmina timeshift
+
+sudo apt install -y ristretto pinta

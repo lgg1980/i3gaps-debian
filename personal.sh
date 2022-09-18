d343a75ec44fb5da8fe3713707378f92486c7823
lgg1980 <63470677+lgg1980@users.noreply.github.com>
2022-09-18 08:11:32 +0200

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -1,11 +1,17 @@
+# Various additional command line things
+sudo apt install mc squashfuse tilix cmatrix iw
 sudo apt install -y cifs-utils hfsplus hfsutils hfsutils-tcltk gufw gparted bleachbit gdebi dosbox apt-transport-https
-
 sudo apt install -y rar p7zip-rar xarchiver p7zip-full unace zip unzip sharutils uudeview mpack arj cabextract file-roller zstd
 sudo apt install -y xclip libnotify-bin
 
+# Multimedia
 sudo apt install -y clementine qbittorrent kid3 vlc youtube-dl audacity kdenlive handbrake
 sudo apt install -y obs-studio simplescreenrecorder
 
+# Basic
 sudo apt install -y filezilla keepassxc thunderbird thunderbird-l10n-es-es webext-tbsync telegram-desktop remmina timeshift
-
+# Image
 sudo apt install -y ristretto pinta
+
+# Required
+pip3 install netifaces

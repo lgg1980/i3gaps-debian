46391ad8b53b50ba650710bf91d38de006bc93e3
lgg1980 <63470677+lgg1980@users.noreply.github.com>
2022-09-18 13:41:41 +0200

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -1,8 +1,12 @@
 # Various additional command line things
 sudo apt install mc squashfuse cmatrix iw playerctl
-sudo apt install -y cifs-utils hfsplus hfsutils hfsutils-tcltk gufw gparted bleachbit gdebi dosbox apt-transport-https
+sudo apt install -y cifs-utils hfsplus hfsutils hfsutils-tcltk exfat-utils apt-transport-https
 sudo apt install -y rar p7zip-rar xarchiver p7zip-full unace zip unzip sharutils uudeview mpack arj cabextract file-roller zstd
 sudo apt install -y xclip libnotify-bin
+
+sudo apt install -y gufw gparted bleachbit gdebi dosbox
+
+sudo apt install -y fonts-powerline fonts-liberation2
 
 # Multimedia
 sudo apt install -y clementine qbittorrent kid3 vlc youtube-dl kdenlive handbrake
@@ -15,6 +19,9 @@ sudo apt install -y ristretto pinta flameshot
 
 # Required
 pip3 install netifaces
+
+# fun
+sudo apt install -y etw openttd
 
 cd ~/
 mkdir conty

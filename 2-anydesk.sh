a30afd33fae94e0307b440e958924cccd9472756
lgg1980 <63470677+lgg1980@users.noreply.github.com>
2023-06-20 17:13:34 +0200

    Update 2-anydesk.sh
    sudo to doas

--- 2-anydesk.sh
+++ 2-anydesk.sh
@@ -1,4 +1,4 @@
-sudo wget -O- https://keys.anydesk.com/repos/DEB-GPG-KEY | gpg --dearmor | sudo tee /usr/share/keyrings/anydesk.gpg
+doas wget -O- https://keys.anydesk.com/repos/DEB-GPG-KEY | gpg --dearmor | sudo tee /usr/share/keyrings/anydesk.gpg
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/anydesk.gpg] http://deb.anydesk.com/ all main' | sudo tee /etc/apt/sources.list.d/anydesk.list
-sudo apt update
-sudo apt install anydesk -y
+doas apt update
+doas apt install anydesk -y

bc622d233467e15c462647afbee2d5ccf26bc54a
lgg1980 <63470677+lgg1980@users.noreply.github.com>
2023-06-20 17:14:15 +0200

    Update vmware-fix.sh

--- vmware-fix.sh
+++ vmware-fix.sh
@@ -1,5 +1,5 @@
-sudo git clone -b workstation-$( grep player.product.version /etc/vmware/config | sed '/.*\"\(.*\)\".*/ s//\1/g' ) https://github.com/mkubecek/vmware-host-modules.git /opt/vmware-host-modules/
+doas git clone -b workstation-$( grep player.product.version /etc/vmware/config | sed '/.*\"\(.*\)\".*/ s//\1/g' ) https://github.com/mkubecek/vmware-host-modules.git /opt/vmware-host-modules/
 cd /opt/vmware-host-modules/
 
-sudo make
-sudo make install
+doas make
+doas make install

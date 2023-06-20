bc96d23ae3382dcc7855a4b06ebb63b2fbafcf48
lgg1980 <63470677+lgg1980@users.noreply.github.com>
2023-06-20 17:12:06 +0200

    Update personal.sh
    sudo to doas

--- personal.sh
+++ personal.sh
@@ -1,27 +1,27 @@
 # Various additional command line things
-sudo apt install mc squashfuse cmatrix iw playerctl
-sudo apt install -y cifs-utils hfsplus hfsutils hfsutils-tcltk exfat-utils apt-transport-https gnupg2
-sudo apt install -y rar p7zip-rar xarchiver p7zip-full unace zip unzip sharutils uudeview mpack arj cabextract file-roller zstd
-sudo apt install -y xclip libnotify-bin
+doas apt install mc squashfuse cmatrix iw playerctl
+doas apt install -y cifs-utils hfsplus hfsutils hfsutils-tcltk exfat-utils apt-transport-https gnupg2
+doas apt install -y rar p7zip-rar xarchiver p7zip-full unace zip unzip sharutils uudeview mpack arj cabextract file-roller zstd
+doas apt install -y xclip libnotify-bin
 
-sudo apt install -y gufw gparted bleachbit gdebi dosbox 
-sudo apt install -y flatpak
+doas apt install -y gufw gparted bleachbit gdebi dosbox 
+doas apt install -y flatpak
 
-sudo apt install -y fonts-powerline fonts-liberation2
-sudo apt install -y numix-gtk-theme numix-icon-theme numix-icon-theme-circle
+doas apt install -y fonts-powerline fonts-liberation2
+doas apt install -y numix-gtk-theme numix-icon-theme numix-icon-theme-circle
 
 # Multimedia
-sudo apt install -y clementine qbittorrent kid3 vlc youtube-dl kdenlive handbrake
-sudo apt install -y obs-studio simplescreenrecorder
+doas apt install -y clementine qbittorrent kid3 vlc youtube-dl kdenlive handbrake
+doas apt install -y obs-studio simplescreenrecorder
 
 # Basic
-sudo apt install -y galculator keepassxc remmina timeshift
+doas apt install -y galculator keepassxc remmina timeshift
 # Internet
-sudo apt install -y qutebrowser filezilla thunderbird thunderbird-l10n-es-es webext-tbsync telegram-desktop
+doas apt install -y qutebrowser filezilla thunderbird thunderbird-l10n-es-es webext-tbsync telegram-desktop
 # Image
-sudo apt install -y ristretto pinta flameshot
+doas apt install -y ristretto pinta flameshot
 # lock screen
-sudo apt install -y xss-lock i3lock
+doas apt install -y xss-lock i3lock
 
 # flatpak
 
@@ -36,15 +36,15 @@ FLATPAK_LIST=(
 )
 
 # add flathub repository
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+doas flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo apt update
+doas apt update
 
 # add third party software
  
 # update repositories
 
-sudo apt-get update -yq
+doas apt-get update -yq
 
 for flatpak_name in ${FLATPAK_LIST[@]}; do
 	if ! flatpak list | grep -q $flatpak_name; then
@@ -57,24 +57,24 @@ done
 # grab codium
 echo "Grabbing VSCode without telemetry"
 sleep 1
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
-echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list 
-sudo apt update -yq && sudo apt install codium -yq
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | doas dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
+echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | doas tee --append /etc/apt/sources.list.d/vscodium.list 
+doas apt update -yq && doas apt install codium -yq
 # -----------------------------------------------------------------------------------------------------------------------
 wget https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb
-sudo dpkg -i ./onlyoffice-desktopeditors_amd64.deb
+doas dpkg -i ./onlyoffice-desktopeditors_amd64.deb
 # -----------------------------------------------------------------------------------------------------------------------
 wget https://github.com/manga-download/hakuneko/releases/download/v6.1.7/hakuneko-desktop_6.1.7_linux_amd64.deb
-sudo dpkg -i ./hakuneko-desktop_6.1.7_linux_amd64.deb
+doas dpkg -i ./hakuneko-desktop_6.1.7_linux_amd64.deb
 # -----------------------------------------------------------------------------------------------------------------------
 # Required
 pip3 install netifaces suntime python-dateutil
 
-sudo apt --fix-broken install # onlyoffice dependencies
+doas apt --fix-broken install # onlyoffice dependencies
 
 # fun
 # commented on 18/09/2022 must be 'relaxed' until real install, working great on VM tho
-# sudo apt install -y etw openttd
+# doas apt install -y etw openttd
 
 # cd ~/
 # mkdir conty

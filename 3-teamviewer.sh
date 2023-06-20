440f1424c1012a676e29cf071b65ebc31d308abb
lgg1980 <63470677+lgg1980@users.noreply.github.com>
2023-06-20 17:13:09 +0200

    Update 3-teamviewer.sh
    sudo to doas

--- 3-teamviewer.sh
+++ 3-teamviewer.sh
@@ -1,4 +1,4 @@
 wget -O- https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc | gpg --dearmor | sudo tee /usr/share/keyrings/teamview.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/teamview.gpg] http://linux.teamviewer.com/deb stable main" \ | sudo tee /etc/apt/sources.list.d/teamviewer.list
-sudo apt update
-sudo apt install teamviewer -y
+doas apt update
+doas apt install teamviewer -y

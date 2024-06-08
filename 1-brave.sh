2d892189e6da519cfe48bb574fa7946cba47a6fb
lgg1980 <63470677+lgg1980@users.noreply.github.com>
2024-06-08 13:09:26 +0200

    Update 1-brave.sh

--- 1-brave.sh
+++ 1-brave.sh
@@ -1,5 +1,5 @@
-sudo apt install software-properties-common apt-transport-https wget ca-certificates gnupg2 -y
-wget -O- https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/brave-browser-archive-keyring.gpg
-echo deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
+apt install software-properties-common apt-transport-https wget ca-certificates gnupg2 -y
+wget -O- https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | gpg --dearmor | tee /usr/share/keyrings/brave-browser-archive-keyring.gpg
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main | tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+apt install brave-browser -y
